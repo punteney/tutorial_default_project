ed36415985e7ba2896391e5dd431bd375c0af0f8
James Punteney <punteney@gmail.com>
2010-03-28 14:55:41 -0400

    Updates to the default project setup

--- project/settings/__init__.py
+++ project/settings/__init__.py
@@ -3,17 +3,17 @@ import os
 ##################
 # Importing the appropriate settings file for this environment
 ##################
-ENVIRONMENT = os.getenv("CONFIG_IDENTIFIER")
-if not ENVIRONMENT:
-    print("******  No Environment is specified  ********")
-    ENVIRONMENT = 'dev'
+ENVIRONMENT = os.getenv("DJANGO_ENVIRONMENT")
 
 if ENVIRONMENT == 'production':
     from production import *
-    print("Production settings enabled")
-elif ENVIRONMENT == 'dev':
-    from dev import *
-    print("Dev settings enabled")
 elif ENVIRONMENT == 'staging':
     from staging import *
-    print("Staging settings enabled")
+else:
+    from dev import *
+
+try:
+    from local import *
+except ImportError:
+    # No local settings which is fine
+    pass
